1a43176e014561d79934b8bce9e604e053eb34d5
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-02-26 21:18:42 +0100

    Adding the logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Link from "next/link";
 
 const navbar = () => {
-    return <div>navbar</div>;
+    return (
+        <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+            {/*Logo*/}
+            <div className="">
+                <Link
+                    href="/"
+                    className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+                >
+                    <span className="text-white mr-1">Gergő</span>
+                    <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
+                        Gyenes
+                    </span>
+                </Link>
+            </div>
+            {/*Responsive Menu*/}
+            <div className="">2</div>
+        </div>
+    );
 };
 
 export default navbar;
