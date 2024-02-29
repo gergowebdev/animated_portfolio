10f632f7ac7b2c2006bbccad533b8ef426205aeb
Gyenes Gergo <gergo.gyenes@gmail.com>
2024-02-29 22:32:17 +0100

    Starting animating the hamburger menu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import NavLinks from "./NavLinks";
+import { motion } from "framer-motion";
 
 const links = [
     { url: "/", title: "Home" },
@@ -14,6 +15,35 @@ const links = [
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
+
+    const topVariants = {
+        closed: {
+            rotate: 0,
+        },
+        opened: {
+            rotate: 45,
+            backgroundColor: "rgb(255,255,255)",
+        },
+    };
+
+    const centerVariants = {
+        closed: {
+            opacity: 1,
+        },
+        opened: {
+            opacity: 0,
+        },
+    };
+
+    const bottomVariants = {
+        closed: {
+            rotate: 0,
+        },
+        opened: {
+            rotate: -45,
+            backgroundColor: "rgb(255,255,255)",
+        },
+    };
 
     return (
         <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
@@ -69,9 +99,21 @@ const Navbar = () => {
                     className="w-10 h-8 flex flex-col justify-between z-50 relative"
                     onClick={() => setOpen(!open)}
                 >
-                    <div className="w-10 h-1 bg-white rounded"></div>
-                    <div className="w-10 h-1 bg-white rounded"></div>
-                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <motion.div
+                        variants={topVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded"
+                    ></motion.div>
+                    <motion.div
+                        variants={centerVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded"
+                    ></motion.div>
+                    <motion.div
+                        variants={bottomVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded"
+                    ></motion.div>
                 </button>
                 {/*Menu List*/}
                 {open && (
